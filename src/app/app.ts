9722663abcd6fcccdeeed4ad17aa964d12bc34c8
Camilo Garcia <hernancgarcia3@gmail.com>
2026-01-21 14:10:45 -0500

    [+]pages: Se crean las carpetas de home, login y register. Con sus respectivas rutas.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,23 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { Header } from './features/shared/layout/header/header';
 import { Footer } from './features/shared/layout/footer/footer';
-import { Counter } from "./features/components/counter/counter";
+import { Home } from "./features/pages/home/home";
+import { Login } from './features/pages/login/login';
+import { Register } from "./features/pages/register/register";
+import { Error } from './features/pages/error/error';
 
 @Component({
   selector: 'app-root',         //Es el componente que permite visualizar la aplicación.
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [RouterOutlet, Header, Footer, Counter],    //Importa las características, ya sean de terceros o propias.
+  imports: [RouterOutlet,
+    Header,
+    Footer,
+    Home,
+    Login,
+    Register,
+    Error
+  ],     //Importa las características, ya sean de terceros o propias.
 })
 export class App {
   protected readonly title = signal('frontend');

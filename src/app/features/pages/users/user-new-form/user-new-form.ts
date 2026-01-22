6bfd983645a6e2522c0a93599bad463d3598d3a1
Camilo Garcia <hernancgarcia3@gmail.com>
2026-01-22 16:13:10 -0500

    [+] user-new-form.ts: Se agregan los Validators. [+] user-new-form.html: se agregarn los mensajes de los campos del formularios requeridos.

--- src/app/features/pages/users/user-new-form/user-new-form.ts
+++ src/app/features/pages/users/user-new-form/user-new-form.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpUsers } from '../../../../core/services/http-users';
-
+import { required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-user-new-form',
@@ -14,28 +14,28 @@ export class UserNewForm {
   constructor(private httpUsers: HttpUsers) {
   //Definie la estructura del formulario
     this.formData = new FormGroup({
-      fullName: new FormControl(''),
-      username: new FormControl(''),
-      email: new FormControl(''),
-      password: new FormControl(''),
+      fullName: new FormControl('', [ Validators.required ]),
+      username: new FormControl('', [ Validators.required ]),
+      email: new FormControl('', [ Validators.required, Validators.email ]),
+      password: new FormControl('', [ Validators.required, Validators.minLength(8),  ]),
       phone: new FormControl(''),
       role: new FormControl('Cliente'),
       isActive: new FormControl(true),
     });
   }
   onSubmit() {
-  console.log(this.formData.value);
-  this.httpUsers.createUser(this.formData.value).subscribe({
-    next: (data) => {
-      console.log('Usuario creado exitosamente', data);
-    },
-    error: (error) => {
-      console.error('Error, usuario no creado', error);
-    },
-    complete: () => {
-      console.log('Solicitud de creación de usuario completada');
-    }
-  });
-}
+    console.log(this.formData.value);
+    this.httpUsers.createUser(this.formData.value).subscribe({
+      next: (data) => {
+        console.log('Usuario creado exitosamente', data);
+      },
+      error: (error) => {
+        console.error('Error, usuario no creado', error);
+      },
+      complete: () => {
+        console.log('Solicitud de creación de usuario completada');
+      }
+    });
+  }
 }
 

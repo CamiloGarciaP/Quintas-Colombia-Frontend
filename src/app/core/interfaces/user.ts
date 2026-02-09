3799020233007f290d98fd5229589850968e3ba3
Camilo Garcia <hernancgarcia3@gmail.com>
2026-02-09 13:10:04 -0500

    [+]Se corrigen detalles mínimos

--- src/app/core/interfaces/user.ts
+++ src/app/core/interfaces/user.ts
@@ -6,6 +6,6 @@ export interface User {
     phone?: string;
     role?: string;
     isActive?: boolean;
-    crateAt?: Date;
+    createAt?: Date;
     updateAt?: Date;
 }
--- src/app/core/services/http-auth.ts
+++ src/app/core/services/http-auth.ts
@@ -3,19 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { User } from '../interfaces/user';
 import { AuthResponse } from '../interfaces/auth-response';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpAuth {
+  private apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
-  register(credentials: User) : Observable<User> {
-    return this.http.post<User>( 'http://localhost:3000/api/v1/auth/register', credentials);
+  register(credentials: User): Observable<User> {
+    return this.http.post<User>(`${this.apiUrl}/auth/register`, credentials);
   }
 
-  login(credentials: Partial<User>) : Observable<AuthResponse> {
-    return this.http.post<AuthResponse>( 'http://localhost:3000/api/v1/auth/login', credentials);
+  login(credentials: Partial<User>): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/login`, credentials);
   }
   
   saveLocalStorageData( token: string , userData: User){

578d088fb51ee9ff915053b507e99b18b8500e36
Camilo Garcia <hernancgarcia3@gmail.com>
2026-02-26 18:45:08 -0500

    [+]Se arreglan los archivos de dashboard en el .ts y .html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
+import { Component, signal, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from "@angular/router";
 import { Header } from './features/shared/layout/header/header';
 import { Footer } from './features/shared/layout/footer/footer';
 
@@ -9,6 +9,27 @@ import { Footer } from './features/shared/layout/footer/footer';
   styleUrl: './app.css',
   imports: [RouterOutlet, Header, Footer ],     //Importa las características, ya sean de terceros o propias.
 })
-export class App {
+export class App implements OnInit {    //Implemenctación del OnInit
   protected readonly title = signal('frontend');
+  
+  //Variable para mostrar/oculatr el diseño público.
+  showPublicLayout: boolean = true;
+
+  //Inyecta el servicio de Roter
+  constructor(private router: Router) {}
+
+  //Se inicia el componente
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      //Detecta el fin de la navegación
+      if (event instanceof NavigationEnd) {
+        //Si la URL incluye '/dashboard', oculta el diseño público
+        if(event.urlAfterRedirects.includes('/dashboard')){
+          this.showPublicLayout = false;
+        }else{
+          this.showPublicLayout = true;
+        }
+      }
+    });  
+  }
 }
